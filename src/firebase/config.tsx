f3791ce54a5d24c600fc284a542bef4db0fc5f52
AlecsLouis <louisalecs@gmail.com>
2025-06-30 13:14:51 +0800

    Set up Firebase config with .env and update project

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -12,14 +12,15 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-  apiKey: "AIzaSyBoSPj1fd2-EvbX70cWv5fxQarBdt8k0HQ",
-  authDomain: "todolist-4c316.firebaseapp.com",
-  databaseURL: "https://todolist-4c316-default-rtdb.firebaseio.com", // Make sure this is correct
-  projectId: "todolist-4c316",
-  storageBucket: "todolist-4c316.firebasestorage.app",
-  messagingSenderId: "145271165749",
-  appId: "1:145271165749:web:7f2e15e932b3876c4af27c"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+console.log("FIREBASE_API_KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
